acceba7b9afb15a89fcab534c6a0fe96fbca1347
Seggy Umboh <seggy.umboh@gmail.com>
2009-04-20 02:11:36 -0700

    Switch to yui css, sharethis, uservoice

---
+++ content/js/thirdparty.js
@@ -0,0 +1,15 @@
+try {
+  var pageTracker = _gat._getTracker("UA-4408061-1");
+  pageTracker._trackPageview();
+} catch(err) {}
+
+UserVoice.Tab.show({ 
+  key: 'hopkinsbakery',
+  host: 'hopkinsbakery.uservoice.com', 
+  forum: 'general', 
+  alignment: 'left', /* 'left', 'right' */
+  background_color:'#f00', 
+  text_color: 'white', /* 'white', 'black' */
+  hover_color: '#06C',
+  lang: 'en' /* 'en', 'de', 'nl', 'es', 'fr' */
+})
